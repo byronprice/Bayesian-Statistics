f7900f8c3741bccf9d367a8432b40bbfe5d44575
byronprice <byron.h.price@gmail.com>
2019-12-17 22:59:51 -0500

    correct cholesky decomp

--- AbsExpCov1D.m
+++ AbsExpCov1D.m
@@ -1,4 +1,4 @@
-function [K,Kinv,logdeterminant] = AbsExpCov1D(sigsquare,rho,d)
+function [K,Kinv,logdeterminant,cholInv] = AbsExpCov1D(sigsquare,rho,d)
 % AbsExpCov.m
 %  Code to get the absolute value exponential covariance matrix (the Matern
 %   covariance for nu=1/2), along with its inverse and determinant. This
@@ -12,12 +12,13 @@ function [K,Kinv,logdeterminant] = AbsExpCov1D(sigsquare,rho,d)
 %       d - dimensionality of the kernel
 %OUTPUT: K - the covariance matrix / kernel
 %        Kinv - the inverse of the covariance
-%        determinant - the matrix determinant, which is used to get the
+%        logdeterminant - the log matrix determinant, which is used to get the
 %         inverse extremely quickly.
+%        cholInv - Cholesky decomposition of the inverse
 
 % Created: 2018/01/19, 24 Cummington Mall, Boston, MA
 %   Byron Price, PhD Candidate Boston University
-% Updated: 2018/08/16
+% Updated: 2019/12/17
 %   By: Byron Price
 
 [K] = GetMain(sigsquare,rho,d);
@@ -25,6 +26,7 @@ function [K,Kinv,logdeterminant] = AbsExpCov1D(sigsquare,rho,d)
 
 [Kinv] = GetInverse(sigsquare,rho,d);
 
+[cholInv] = GetCholeskyInv(sigsquare,rho,d);
 end
 
 function [K,D] = GetMain(sigsquare,rho,d)
@@ -57,10 +59,9 @@ function [inverse] = GetInverse(sigsquare,rho,d)
 inverse = sparse(d,d);oneVec = ones(d-1,1);
 mainDiagInds = find(eye(d));
 
-[det2] = GetDet(sigsquare,rho,2);
-[det3] = GetDet(sigsquare,rho,3);
+[det3,ratio] = GetDet(sigsquare,rho,3);
 
-inverse(1,1) = exp(det2-det3);inverse(d,d) = exp(det2-det3);
+inverse(1,1) = exp(-ratio);inverse(d,d) = exp(-ratio);
 mainDiagInds = mainDiagInds(2:end-1);
 
 % inverse(mainDiagInds) = ((sigsquare.^2).*(1-exp(-2./rho).^2))./det3;
@@ -71,4 +72,18 @@ temp = ((sigsquare.^2).*(exp(-1./rho)).*(exp(-2./rho)-1))./exp(det3);
 
 inverse(logical(diag(oneVec,1))) = temp;
 inverse(logical(diag(oneVec,-1))) = temp;
+end
+
+function [cholInv] = GetCholeskyInv(sigsquare,rho,d)
+cholInv = sparse(d,d);oneVec = ones(d-1,1);
+
+[det3,ratio] = GetDet(sigsquare,rho,3);
+
+cholInv(logical(eye(d))) = sqrt(exp(-ratio));
+
+invDiag = exp(2*log(sigsquare)+log(1-exp(-4/rho))-det3);
+
+cholInv(logical(diag(oneVec,1))) = -sqrt(invDiag-exp(-ratio));
+
+cholInv(d,d) = sqrt(2*exp(-ratio)-invDiag);
 end
